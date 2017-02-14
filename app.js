51f906f5eb6606f4026b6995a852df861aa50240
tkopp8809 <tkopp8809@yahoo.com>
2017-02-14 00:37:12 -0800

    Better route

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient
 var http = require('http');
-var grim = []
+var path = require('path');
+
 MongoClient.connect('mongodb://localhost:27017/library', function (err, db) {
   if (err) throw err
 
@@ -15,17 +16,9 @@ MongoClient.connect('mongodb://localhost:27017/library', function (err, db) {
 })
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/about', function (req, res) {
-  getBookByTitle(grim);
-  res.json(grim)
-})
+app.use('/', express.static(__dirname + '/User'));
+
 
 app.listen(3000, function(){
-  getBookByTitle(grim);
-  console.log(grim);
   console.log(('Example app listening on port 3000!'));
 });
-
-var getBookByTitle = function (grim){
-  db.test.find({title : 'Grimm Brothers'}).toArray(grim)
-}
